6f9f84076f7bcbaaa7e28ae7194e780e879e1687
zcfrank1st <zcfrank1st@sina.com>
2016-01-06 16:57:44 +0800

    add parse method

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,5 @@
 import bean.Activity;
 import bean.Rule;
-import com.google.gson.Gson;
 import grammar.generated.ActivityLexer;
 import grammar.generated.ActivityParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -15,19 +14,57 @@ import java.util.List;
  */
 public class Parser {
     public static void main(String[] args) {
-        String list = "AND { 满五减十 @ QUANTITY >= 5 -> TOTAL_PRICE -10 满五减十 @ QUANTITY >= 10 -> TOTAL_PRICE -10 满五减十 @ TOTAL_PRICE >= 10000 -> ITEM 1,2,4}";
+//        String list = "AND { 满五减十 @ QUANTITY >= 5 -> TOTAL_PRICE -10 满五减十 @ QUANTITY >= 10 -> TOTAL_PRICE -10 满五减十 @ TOTAL_PRICE >= 10000 -> ITEM 1,2,4}";
+//
+//        ActivityLexer lexer = new ActivityLexer(new ANTLRInputStream(list));
+//
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//
+//        ActivityParser parser = new ActivityParser(tokens);
+//
+//        ParseTree tree = parser.main();
+//
+////        ParseTreeWalker walker = new ParseTreeWalker();
+////
+////        walker.walk(new ActivityProcessor(), tree);
+//
+//        ActivityVisitor activityVisitor = new ActivityVisitor();
+//        activityVisitor.visit(tree);
+//
+//        String content = activityVisitor.getContent();
+//        String[] parts = content.split("@");
+//
+//        List<Rule> rules = new ArrayList<>();
+//        Activity activity = new Activity();
+//        String type = parts[0];
+//
+//        activity.setType(type);
+//
+//        for (int i=0; i< (parts.length - 1) / 6; i++) {
+//            Rule rule = new Rule();
+//            rule.setName(parts[i * 6 + 1]);
+//            rule.setLabel(parts[i * 6 + 2]);
+//            rule.setOpt(parts[i * 6 + 3]);
+//            rule.setThreshold(parts[i * 6 + 4]);
+//            rule.setHost(parts[i * 6 + 5]);
+//            rule.setEffect(parts[i * 6 + 6]);
+//            rules.add(rule);
+//        }
+//
+//        activity.setRules(rules);
+//
+//        Gson gson = new Gson();
+//        System.out.println(gson.toJson(activity));
+    }
 
-        ActivityLexer lexer = new ActivityLexer(new ANTLRInputStream(list));
+    private Activity buildActivity(String s) {
+        ActivityLexer lexer = new ActivityLexer(new ANTLRInputStream(s));
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         ActivityParser parser = new ActivityParser(tokens);
 
         ParseTree tree = parser.main();
-
-//        ParseTreeWalker walker = new ParseTreeWalker();
-//
-//        walker.walk(new ActivityProcessor(), tree);
 
         ActivityVisitor activityVisitor = new ActivityVisitor();
         activityVisitor.visit(tree);
@@ -54,7 +91,10 @@ public class Parser {
 
         activity.setRules(rules);
 
-        Gson gson = new Gson();
-        System.out.println(gson.toJson(activity));
+        return activity;
+    }
+
+    public Activity parseActivity(String s) {
+        return buildActivity(s);
     }
 }
